4421cad981efd4924a6f2ba48f1a97c363975518
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-09 15:33:49 -0600

    Create Punto 8-Codigo.py
    8- Definir una función (), que tome tres números como argumentos y devuelva el
    mayor de ellos.

---
+++ Punto 8-Codigo.py
@@ -0,0 +1,12 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("----------------Punto 8-----------------")
+def maxi(A,B,C):#definamos una funsion que calcule el numero mayor de 3 dijitos y escriba el valor maximo
+    Max = max(A, B, C)
+    print("El numero maximo es:",Max)
+A = float(input("Agregar valor:"))#con esto se caturan los valores a definir
+B = float(input("Agregar valor:"))
+C = float(input("Agregar valor:"))
+maxi(A,B,C)#con esto se invoca la funsion
+print("--------------------------------------")
+print("Objetivo: Definir una función (), que tome tres números como argumentos y devuelva el mayor de ellos.")
+print("Resultado: se bio el valor maximo de los 3 numeros elegidos.\n")
