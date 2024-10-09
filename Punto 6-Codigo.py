843a2d4018341c5a3f8d20fbe85168ce3734fdcd
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-09 15:31:45 -0600

    Create Punto 6-Codigo.py
    6- Capturar dirección de email. Desplegar mensaje si la dirección es válida o no, siendo que una función lo revisar por tener la @ solo así es valida

---
+++ Punto 6-Codigo.py
@@ -0,0 +1,10 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("----------------Punto 6-----------------")
+email = input("Cual es tu diresion de correo:")
+if '@' in email:
+    print("Este correo es valido")
+else:
+    print("Este correo no es valido")
+print("--------------------------------------")
+print("Objetivo: Capturar dirección de email. Desplegar mensaje si la dirección es válida o no, siendo que una función lo revisar por tener la @ solo así es valida.")
+print("Resultado: si hay un @ el correo es valido y sino no es valido, y se logro ver el resultado.\n")
