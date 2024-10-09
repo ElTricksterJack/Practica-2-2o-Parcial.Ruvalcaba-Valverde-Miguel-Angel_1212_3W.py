cfe4f7cbda150ed4bc809b17cc9849cc4761bc0c
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-09 15:33:18 -0600

    Create Punto 7-Codigo.py
    7- Función que de un string, regrese la longitud de la última palabra. Las palabras tienen separación por uno o más espacios.

---
+++ Punto 7-Codigo.py
@@ -0,0 +1,8 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("----------------Punto 7-----------------")
+pala = str(input("Escoje una frase o palabra:"))#HOLA niños caturemos una palabra
+print("Cantidad de letras de la palabra:",len(pala))#bien ahora meustrenla en pantalla con el comando len
+print("Tambien cuentan los espasios")#adios.
+print("--------------------------------------")
+print("Objetivo: Función que de un string, regrese la longitud de la última palabra. Las palabras tienen separación por uno o más espacios.")
+print("Resultado: se logro ver la longitud de la pabra usando el comando len\n")
