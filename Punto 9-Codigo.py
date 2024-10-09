4f47672323b7f15ea2243f7fb7372e57f689dca0
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-09 15:34:47 -0600

    Create Punto 9-Codigo.py
    9- Escribir una funcion sum() y una función multip() que sumen y multipliquen respectivamente
    todos los números de una lista.

---
+++ Punto 9-Codigo.py
@@ -0,0 +1,18 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("----------------Punto 9-----------------")
+def multi(A,B,C,D,E):
+    mul=A*B*C*D*E
+    print("Al multiplicar todos los valores ingresados da:",mul)
+def suma(A,B,C,D,E):
+    sum=A+B+C+D+E
+    print("Al sumar todos los valores ingresados da:",sum)
+A = float(input("Allade un valor 1:"))
+B = float(input("Allade un valor 2:"))
+C = float(input("Allade un valor 3:"))
+D = float(input("Allade un valor 4:"))
+E = float(input("Allade un valor 5:"))
+suma(A,B,C,D,E)
+multi(A,B,C,D,E)
+print("--------------------------------------")
+print("Objetivo: Escribir una funcion sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista.")
+print("Resultado: Se logro ver el resultado coreptamente\n")
