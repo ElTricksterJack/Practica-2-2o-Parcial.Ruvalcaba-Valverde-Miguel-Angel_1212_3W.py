e73f5b4d47cad888bacb31ab6426637d8067dad7
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-09 15:27:54 -0600

    Create Punto 2-Codigo.py
    Dar un string <nombre> y despliegue un saludo ¡hola <nombre>!.

---
+++ Punto 2-Codigo.py
@@ -0,0 +1,8 @@
+print("\nRuvalcaba Valverde Mguel Angel_1212_3W")
+print("----------------Punto 2-----------------")
+name = input("Escribe tu nombre:")#SE PERMITE CAPTURAr las 2 variables
+ape = input("Escribe tus apellidos:")#AAAAA porque tengo que explicar algo tan basico
+print("¡hola ",name,ape,"!")#se muestra en pantalla
+print("--------------------------------------")
+print("Objetivo: Dar un string <nombre> y despliegue un saludo ¡hola <nombre>!.")
+print("Resultado: Se logro ver el nombre y sus apellidos en el saludo de manera correpta.\n")
